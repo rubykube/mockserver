ad96f44c78e80f58cb6915d8f5b1585509fe9364
Camille <cmeulien@heliostech.fr>
2018-12-04 18:44:57 +0100

    Improve startup script

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 var http = require('http');
 var mockserver = require('mockserver');
-const port = 9001;
+var argv = require('yargs').argv;
+var colors = require('colors')
+var port = argv.p || argv.port || 9001;
+var help = argv.h || argv.help;
+var verbose = true;
 
-http.createServer(mockserver('src/mocks')).listen(port, (error) => {
-  if (error) {
-    console.log("Unhandled exception", error);
-    return;
-  }
+if (help) {
+  console.log([
+    "Usage:",
+    "  index.js [-q] -p PORT",
+    "",
+    "Options:",
+    "  -p, --port=PORT    - Port to listen on",
+    "",
+    "Example:",
+    "  mockserver -p 8080"
+  ].join("\n"));
+} else {
+  http.createServer(mockserver("src/mocks", verbose)).listen(port, (error) => {
+    if (error) {
+      console.log("Unhandled exception", error);
+      return;
+    }
 
-  console.log(`Server is listening on port ${port}`);
-})
+    if (verbose) {
+      console.log('Mockserver serving mocks {'
+                  + 'verbose'.yellow + ':' + (verbose && 'true'.green || 'false')
+                  + '} ' + `http://localhost:${port}`.green);
+    }
+  })
+}
