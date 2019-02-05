06bbc7f4bd86e27df5c454e1c73e78817be5ac88
Camille <cmeulien@heliostech.fr>
2019-02-05 15:33:55 +0100

    Fix: k-line function to have negative periods

--- src/mocks-v2/api/v2/peatio/public/markets/k-line.js
+++ src/mocks-v2/api/v2/peatio/public/markets/k-line.js
@@ -3,11 +3,17 @@ let k = [];
 
 const parseQuery = (url) => {
     let vars = {};
-    url.substring(queryIndex+1).split("&").forEach((chunk) => {
+    url.substring(queryIndex + 1).split("&").forEach((chunk) => {
         const arr = chunk.split("=");
         vars[arr[0]] = arr[1];
     });
     return vars;
+}
+
+const timeToPrice = (time) => {
+    const fakePeriod = 86400;
+    const step = 100;
+    return (step / 4 * (1 + Math.cos((time / fakePeriod) * 2 * Math.PI)) + parseInt(step * time / fakePeriod));
 }
 
 if (queryIndex != -1) {
@@ -15,8 +21,6 @@ if (queryIndex != -1) {
     const period = parseInt(vars.period) * 60;
     const time_to = parseInt(vars.time_to);
     const time_from = parseInt(vars.time_from);
-    const step = 100;
-    const fakePeriod = 86400;
     let time = time_from;
     let open;
     let high;
@@ -24,10 +28,10 @@ if (queryIndex != -1) {
     let close;
 
     while (time < time_to) {
-        open = step / 4 * (1 + Math.cos((time / fakePeriod) * 2 * Math.PI)) + parseInt(step * time / fakePeriod);
-        close = open + 4;
+        open = timeToPrice(time);
+        close = timeToPrice(time + period);
         high = close + 2;
-        low  = open - 2;
+        low = open - 2;
         k.push([time, open, high, low, close]);
         time += period;
     }
